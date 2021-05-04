1df777c9c0ca84d799457994bc87dc738a41e06c
Natalie <theliteshado@icloud.com>
2021-05-04 02:18:53 -0400

    lyrics working

--- controllers/lyricController.js
+++ controllers/lyricController.js
@@ -12,22 +12,42 @@ lyricController.create = async (req, res) => {
     try {
         const user = await models.user.findOne({
           where: {
-            id: req.body.id
+            id: req.body.userId
           }
         })
         console.log(user)
 
-        let lyric = await user.createlyric({
+
+        // let songNumber = await models.song.findOne({
+        //     where: {
+        //         id: req.body.songId
+        //     }
+        // })
+        // let song = await models.song.findOne({
+        //     where: {
+        //         id: req.params.id
+        //     }
+        // })
+        // let lyric = await user.createLyric({
+        //     lyric: req.body.lyric
+        // })
+
+        let lyric = await models.lyric.create({
             lyric: req.body.lyric
         })
 
+        user.addLyric(lyric)
         let song = await models.song.findOne({
             where: {
-                id: req.body.idd
+                id: req.params.id
             }
         })
+        let final = await song.addLyric(lyric)
+        res.json({
+            final, song, user
+        })
         // const userId = user
-        res.json({  message: "nice", user, song, lyric })
+        res.json({  message: "nice", user, lyric, song })
     } catch (error) {
         res.status(400).json({ error: error.message })
         console.log(error)
@@ -44,6 +64,7 @@ lyricController.get = async (req, res) => {
         })
 
         let lyrics = await models.lyric.findAll()
+        
         res.json({song, lyrics})
     } catch (error) {
         res.json({error})
